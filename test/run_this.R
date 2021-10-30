f40901f8ec2fb2cf4d9aee2c7e145b48cdaff096
Resul Umit <resulumit@gmail.com>
2021-10-30 15:03:25 +0200

    installs tinytex conditionally

--- test/run_this.R
+++ test/run_this.R
@@ -1,16 +1,16 @@
 # this script prepares your machine for the workshop by downloading/installing ...
-# 1. five r packages,
-# 2. a custom latex distribution, and
+# 1. four r packages,
+# 2. a custom latex distribution if needed, and
 # 3. an r markdown file
 # ... to your machine
 # please run the script only if you agree with these to happen to your machine
 # running it should create a pdf file in your working directory
 
 # 1. install the packages that we need for the workshop
-install.packages(c("rmarkdown", "tinytex", "dplyr", "ggplot2", "stargazer"))
+install.packages(c("rmarkdown", "dplyr", "ggplot2", "stargazer"))
 
-# 2. install the custom latex distribution 
-tinytex::install_tinytex()
+# 2. install the custom latex distribution, if there is none
+if (Sys.which("pdflatex")[[1]] == "") tinytex::install_tinytex()
 
 # 3. download the test file
 download.file(url = "https://raw.githubusercontent.com/resulumit/rmd_workshop/master/test/the_test.Rmd",
@@ -18,9 +18,8 @@ download.file(url = "https://raw.githubusercontent.com/resulumit/rmd_workshop/ma
 
 # 4. attempt to turn the r markdown file into a pdf
 # note: this will run only if the previous steps are successfully completed
-if (all(c("rmarkdown", "tinytex", "dplyr", "ggplot2", "stargazer") %in% 
-        installed.packages()) & 
-    tinytex:::is_tinytex() & 
+if (all(c("rmarkdown", "dplyr", "ggplot2", "stargazer") %in% installed.packages()) & 
+    Sys.which("pdflatex")[[1]] != "" & 
     file.exists(file = "the_test.Rmd")) rmarkdown::render("the_test.Rmd", "pdf_document")
 
 # if everything works, there will appear a new file, titled the_test.pdf, in your working directory
